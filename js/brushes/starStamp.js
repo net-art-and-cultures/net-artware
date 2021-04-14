007aed09283417f97f4dc49941513b5ae45c3f69
nbriz <nickbriz@gmail.com>
2021-04-14 12:16:33 -0500

    changed color

--- js/brushes/starStamp.js
+++ js/brushes/starStamp.js
@@ -3,7 +3,7 @@ window.brushes.starStamp = {
   name: 'star stamp',
   draw: function () {
     if (mouseIsPressed && !window.dragging) {
-      fill(0)
+      fill(255, 0, 0)
       ellipse(mouseX, mouseY, 50)
     }
   }
