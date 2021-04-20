63247b2e61a558b6c8ae87d297c97b6ea11db3b3
bomiverse <bcho2@saic.edu>
2021-04-20 22:55:03 +0900

    revised indentation

--- js/brushes/flower.js
+++ js/brushes/flower.js
@@ -4,13 +4,13 @@ window.brushes.flower = {
   name: 'rainbow flower',
   draw: function () {
     if (mouseIsPressed && !window.dragging) {
-     colorMode(HSB)
-     noStroke()
-     fill(frameCount % 360, 100, 100)
-     translate(mouseX, mouseY)
-     for (let i = 0; i < 10; i ++) {
-         ellipse(0, 30, 30, 50)
-         rotate(PI/3)
+      colorMode(HSB)
+      noStroke()
+      fill(frameCount % 360, 100, 100)
+      translate(mouseX, mouseY)
+      for (let i = 0; i < 10; i ++) {
+        ellipse(0, 30, 30, 50);
+        rotate(PI/3);
       }
     }
   }
