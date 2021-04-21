1dc0615974ff9e63031007642fdf5cbaed6dd81c
jtao7 <jtao7@artic.edu>
2021-04-21 10:01:10 +0900

    Cleaned up the indentation

--- js/brushes/waterWave.js
+++ js/brushes/waterWave.js
@@ -1,30 +1,26 @@
 /* global mouseIsPressed, random, colorMode, fill, stroke, sin, ellipse,
  HSB, frameCount, mouseX, mouseY */
- var start = 0
- var inc = 0.005
- var xoff = 0
-
+let start = 0
+let inc = 0.005
+let xoff = 0
 window.brushes.waterWave = {
   name: 'water wave',
   draw: function () {
     if (mouseIsPressed && !window.dragging) {
       colorMode(RGB)
       stroke(30,random(10,100),(100,150),random(30,255))
-  noFill()
-  translate(0,mouseY)
-  beginShape()
-  vertex(0,height)
-  xoff = start
-  for(var x = 0;x<width; x++){
-   var y = noise(xoff)*(height-100);
-   vertex(x+mouseX,y)
-
-   xoff+=inc
- }
-  endShape()
-
-  start+= 0.01
-
+      noFill()
+      translate(0,mouseY)
+      beginShape()
+      vertex(0,height)
+      xoff = start
+      for(var x = 0;x<width; x++){
+       let y = noise(xoff)*(height-100);
+       vertex(x+mouseX,y)
+       xoff+=inc
+      }
+      endShape()
+      start+= 0.01
     }
   }
 }
